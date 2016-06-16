e51e3b957e1c57aacf0edf2a8007b0ba556983d5
maimai77 <kohta0707@gmail.com>
2016-06-16 18:01:32 +0900

    add spec sample

---
+++ spec/models/sample_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/../rails_helper')
+
+RSpec.describe Issue, type: :model do
+  descibre '#sample' do
+    it { expect(true).to be_truthy }
+  end
+end
